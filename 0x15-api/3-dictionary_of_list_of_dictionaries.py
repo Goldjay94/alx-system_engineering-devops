925637f6e76e4e03ae07e3057a011551b907fa30
Goldjay94 <genevieveezeh786@gmail.com>
2023-04-24 15:36:10 +0000

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+i#!/usr/bin/python3
+"""This script exports data retrieved
+from an api request in JSON format"""
+
+if __name__ == '__main__':
+    import json
+    import requests as rq
+    from sys import argv
+
+    FILENAME = "todo_all_employees.json"
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = rq.get(url + 'users').json()
+    tasks = rq.get(url + 'todos').json()
+    data = {user.get('id'): [{'username': user.get('username'),
+                              'task': task.get('title'),
+                              'completed': task.get('completed'),
+                              }
+            for task in tasks if task.get('userId') == user.get('id')]
+            for user in users}
+    with open(FILENAME, 'w', newline='') as f:
+        json.dump(data, f)
